dad655f752325b6e2f15dd08e5121cdcb857aea6
Filipe Vilas Bôas <59072469+filipesvb@users.noreply.github.com>
2025-11-10 20:48:40 +0000

    chore: added shadcn@3.5.0 to project / feat: added Button component and started SearchInput component

---
+++ app/_components/search-input.tsx
@@ -0,0 +1,11 @@
+import { Input } from "./ui/input";
+
+const SearchInput = () => {
+  return (
+    <div>
+      <Input type="text" placeholder="Search" />
+    </div>
+  );
+};
+
+export default SearchInput;
