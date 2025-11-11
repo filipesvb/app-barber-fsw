4f03451e73a65c2d535958a15fc443800763c69d
Filipe Vilas Boas <fvb.boas@gmail.com>
2025-11-10 23:25:42 -0300

    feat: added Header, Search and Image components

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,21 @@
+import Image from "next/image";
 import Header from "./_components/header";
+import SearchInput from "./_components/search-input";
+import banner from "../public/banner.png";
 
 export default function Home() {
   return (
     <>
       <Header />
+      <div className="space-y-4 px-5">
+        <SearchInput />
+        <Image
+          src={banner}
+          alt="Agende agora!"
+          sizes="100vw"
+          className="h-auto w-full"
+        />
+      </div>
     </>
   );
 }
