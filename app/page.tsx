91bdbe776df1626893106217f94fec453c85a47a
Filipe Vilas Boas <fvb.boas@gmail.com>
2025-11-11 22:43:57 -0300

    feat: add barbershop item component

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,18 @@ import Header from "./_components/header";
 import SearchInput from "./_components/search-input";
 import banner from "../public/banner.png";
 import BookingItem from "./_components/booking-item";
+import { prisma } from "../lib/prisma";
+import BarbershopItem from "./_components/barbershop-item";
 
-export default function Home() {
+export default async function Home() {
+  const recommendedBarbershops = await prisma.barbershop.findMany({
+    orderBy: { name: "asc" },
+  });
+
+  const popularBarbershops = await prisma.barbershop.findMany({
+    orderBy: { name: "desc" },
+  });
+
   return (
     <main>
       <Header />
@@ -25,6 +35,25 @@ export default function Home() {
           date={new Date()}
           serviceName="Corte de barba"
         />
+        <h2 className="text-foreground text-xs font-semibold uppercase">
+          Recomendados
+        </h2>
+        <div className="flex gap-4 overflow-x-auto [&::-webkit-scrollbar]:hidden">
+          {recommendedBarbershops.map((barbershop) => (
+            <BarbershopItem barbershop={barbershop} key={barbershop.id} />
+          ))}
+        </div>
+        <h2 className="text-foreground text-xs font-semibold uppercase">
+          Populares
+        </h2>
+        <div className="flex gap-4 overflow-x-auto [&::-webkit-scrollbar]:hidden">
+          {popularBarbershops.map((barbershop) => (
+            <BarbershopItem barbershop={barbershop} key={barbershop.id} />
+          ))}
+        </div>
+
+        {/* Development only */}
+        <div className="mt-50"></div>
       </div>
     </main>
   );
